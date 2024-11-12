30f880a4857372b0522f5325069be60f990c753e
Yujin <yujin.mar.chung@gmail.com>
2024-11-12 22:02:30 +0900

    좌석 선택할 화면 준비 완료, 이제 상태를 넣어주어야 함

--- lib/seat_bottom.dart
+++ lib/seat_bottom.dart
@@ -4,8 +4,31 @@ class SeatBottom extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      height: 200,
-      color: Colors.brown,
-    );
+        height: 200,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text('선택된 좌석 없음',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            SizedBox(
+              width: 200,
+              height: 56,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                  ),
+                  child: Text('Book now')),
+            )
+          ],
+        ));
   }
 }
